e6b6370b935f13b83d41b976d646d76920af4ea6
SamTF <elseban88@gmail.com>
2022-04-14 18:51:05 +0100

    Created header component

---
+++ web3app/src/components/Header.svelte
@@ -0,0 +1,21 @@
+<!-- This is the header present on top of every page -->
+<!-- Should contain the website logo, links to all pages, and the user's account & balance -->
+
+<!-- JS -->
+<script>
+    import WalletConnect from "./WalletConnect.svelte"
+</script>
+
+
+<!-- HTML -->
+<header>
+    <div class="header-start">
+        <p>FireMen NFT Inc.</p>
+    </div>
+
+    <div class="header-end">
+        <WalletConnect />
+    </div>
+</header>
+
+
