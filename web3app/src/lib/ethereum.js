44c1abec36475e99cc1d362899710b1a08f80357
SamTF <elseban88@gmail.com>
2022-04-18 00:39:39 +0100

    Updated ethereum constants with new changes

--- web3app/src/lib/ethereum.js
+++ web3app/src/lib/ethereum.js
@@ -2,12 +2,13 @@
 
 import { browser } from '$app/env'
 import { ethers } from 'ethers'
-import { FireMenABI, contractAddress } from './constants'
+import { FireMenABI, contractAddress, MarketABI, marketAddress } from './constants'
 
 // Ethereum constants
 let provider = null
 let signer = null
 let contract = null
+let marketContract = null
 
 
 // This has to be run in the browser and not server-side
@@ -18,15 +19,18 @@ if (browser) {
         provider = new ethers.providers.Web3Provider(window.ethereum)
         signer = provider.getSigner() // getting the user's signature to confirm transactions on the blockchain
     
-        // instantiate the contract
+        // instantiate the contracts
         contract = new ethers.Contract(contractAddress, FireMenABI, signer)
+        marketContract = new ethers.Contract(marketAddress, MarketABI, signer)
     
     // Using a default RPC provider, to allow for read-only functions on the smart contract 
     } else {
         provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
         contract = new ethers.Contract(contractAddress, FireMenABI, provider)
+        marketContract = new ethers.Contract(marketAddress, MarketABI, provider)
     }
 }
 
 
-export { provider, signer, contract }
+// Exports
+export { provider, signer, contract,marketContract }
