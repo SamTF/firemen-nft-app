7821d936df67e33c2317cabbe78e21a3d66dd0d7
SamTF <elseban88@gmail.com>
2022-04-25 15:26:56 +0100

    Updated logo on homepage

--- web3app/src/routes/index.svelte
+++ web3app/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
     // Packages
     import { ethers } from 'ethers'
-    import { FireMenABI, contractAddress } from '../lib/constants'
+    import { contractAddress, marketAddress } from '../lib/constants'
     import { contract, provider, signer } from '../lib/ethereum'
     import { onMount } from "svelte";
     
@@ -9,15 +9,16 @@
     import TokenList from '../components/TokenList.svelte'
 
     // debug function to check that the contract loaded successfully
-    const getByteCode = async () => {
-        const bytecode = await provider.getCode(contractAddress)
+    const getByteCode = async (address) => {
+        const bytecode = await provider.getCode(address)
         console.log(bytecode)
     }
 
     // Client-side Start
     onMount(async () => {
         if (window.ethereum) {
-            getByteCode()
+            getByteCode(contractAddress)
+            getByteCode(marketAddress)
 
             // Reloading the page on network changes
             window.ethereum.on('chainChanged', async (_chainId) => {
@@ -28,7 +29,7 @@
 </script>
 
 <main>
-    <img src='/images/svelte.png' alt="Svelte Logo" />
+    <img src='/images/Fireman.webp' alt="Fireman" class="logo" />
     <h1>Firemen NFT App</h1>
 
     <p></p>
